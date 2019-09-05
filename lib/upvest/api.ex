d4e91a1d60107f080f8913472438cdc2a5b724ba
Yao Adzaku <yao.adzaku@gmail.com>
2019-09-05 19:55:43 +0000

    Use patch for update requests
    Also refactor retrieval of collections

--- lib/upvest/api.ex
+++ lib/upvest/api.ex
@@ -3,6 +3,7 @@ defmodule Upvest.API do
     quote do
       import Upvest, only: [request: 4]
       alias Upvest.Client
+      @page_size 100
 
       if :create in unquote(opts) do
         @doc """
@@ -29,7 +30,7 @@ defmodule Upvest.API do
         """
         def update(id, data, client) when is_bitstring(id) do
           resource_url = Path.join(endpoint(), id)
-          request(:put, resource_url, data, client)
+          request(:patch, resource_url, data, client)
         end
       end
 
@@ -38,35 +39,35 @@ defmodule Upvest.API do
         List all #{__MODULE__ |> to_string |> String.split(".") |> List.last()}
         #TODO: support configurable page size
         """
-        def list(count, client) do
-          results = list_n(endpoint(), count, client, [])
+        def list_n(count, client) do
+          results = do_list_n(endpoint(), count, client, [])
           {:ok, results}
         end
 
-        defp list_n(url, count, client, acc) do
+        defp do_list_n(url, count, client, acc) do
           {:ok, resp} = request(:get, url, %{}, client)
           next = Map.get(resp, "next")
 
-          case is_nil(next) or length(acc) >= count do
+          case is_nil(next) or length(acc) == count do
             true ->
               acc
 
             _ ->
               uri = URI.parse(next)
-              params = Map.put(URI.decode_query(uri.query), :page_size, 100)
+              params = Map.put(URI.decode_query(uri.query), :page_size, @page_size)
               next_url = URI.parse(next).path |> String.slice(4..-1)
               next_url = "#{next_url}?#{URI.encode_query(params)}"
-              list_n(next_url, count, client, acc ++ resp["results"])
+              do_list_n(next_url, count, client, acc ++ resp["results"])
           end
         end
 
         def list(client) do
-          results = list(endpoint(), client, [])
+          results = do_list(endpoint(), client, [])
           {:ok, results}
         end
 
-        defp list(url, client, acc) do
-          {:ok, resp} = request(:get, url, %{}, client)
+        defp do_list(url, client, acc) do
+          {:ok, resp} = request(:get, url, %{page_size: @page_size}, client)
           next = Map.get(resp, "next")
 
           case is_nil(next) do
@@ -75,10 +76,10 @@ defmodule Upvest.API do
 
             _ ->
               uri = URI.parse(next)
-              params = Map.put(URI.decode_query(uri.query), :page_size, 100)
+              params = Map.put(URI.decode_query(uri.query), :page_size, @page_size)
               next_url = URI.parse(next).path |> String.slice(4..-1)
               next_url = "#{next_url}?#{URI.encode_query(params)}"
-              list(next_url, client, acc ++ resp["results"])
+              do_list(next_url, client, acc ++ resp["results"])
           end
         end
       end
